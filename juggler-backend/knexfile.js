f1e5413cfa9c126144368baa024b216d37ab0bb1
W. David Raike <wdraike@gmail.com>
2026-05-27 16:48:47 -0400

    fix(docker): exclude .env from builds, use env vars in test knexfile

--- juggler-backend/knexfile.js
+++ juggler-backend/knexfile.js
@@ -73,11 +73,11 @@ module.exports = {
   test: {
     client: 'mysql2',
     connection: {
-      host: '127.0.0.1',
-      port: 3308,
-      user: 'root',
-      password: '',
-      database: 'juggler_test',
+      host: process.env.DB_HOST || '127.0.0.1',
+      port: process.env.DB_PORT || 3306,
+      user: process.env.DB_USER || 'root',
+      password: process.env.DB_PASSWORD || '',
+      database: process.env.DB_NAME || 'juggler_test',
       charset: 'utf8mb4',
       timezone: '+00:00',
       dateStrings: true
